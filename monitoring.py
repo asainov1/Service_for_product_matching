30517145b5b83b69d5e5c8b69df89d17eafdf72c
Alikhan Sainov <40138758+asainov1@users.noreply.github.com>
2025-05-10 15:34:53 +0500

    Update monitoring.py

--- monitoring.py
+++ monitoring.py
@@ -28,20 +28,9 @@ from datetime import datetime
 from pytz import timezone  # Import timezone from pytz
 #%% Коннекшны
 def main ():
-    myconn = mysql.connector.connect(user='sainov_58623', password='VL$%_I4', 
-    port = '9030',
-    host='starrocksdb')
-
-    conn = cx_Oracle.connect(user='CRRT', password='NLP4321###', dsn='dwhnew-db:1521/node124')
 
     proxy_handler = request.ProxyHandler({})
     opener = request.build_opener(proxy_handler)
-
-    myconn_crrt = mysql.connector.connect(
-        host='starrocksdb',#'dwh-dbr5-lp2',
-        port='9030',
-        user='crrt',
-        password='4g1q6Dc=c7') 
 
     # Устанавливаю время
 
@@ -263,12 +252,7 @@ email_recipient_list = receivers
 try:
     monitoring, logging_values = main()
     
-    db_url = 'mysql+mysqlconnector://crrt:4g1q6Dc=c7@starrocksdb:9030'
-    engine = create_engine(db_url) 
-    myconn=mysql.connector.connect(user='crrt',
-        password='4g1q6Dc=c7',
-        port='9030',
-        host='starrocksdb') 
+
 
     new_row = pd.DataFrame(logging_values)
 
